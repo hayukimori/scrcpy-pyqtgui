c36347b046462faab5c8628ff951b1d369cb7f40
hayukimori <69482956+hayukimori@users.noreply.github.com>
2022-02-08 15:03:40 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,127 +1,103 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- 
 
-#Libs: PyQt5, os, PyGame, sys, re, subprocess
-import os, sys, subprocess, re
+#Libs: PyQt5, os, sys, re, subprocess
+import os, sys, re, subprocess;
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from ppadb.client import Client as AdbClient
 
 
-# ==> Show error
-def err_h(details):
-    error_window.show()
-
-    if details == "no devices":
-        error_window.text.setText("No devices connected")
-    else:
-        error_window.text.setText(details)
-
-
-
-
-
-# ==> Get Devices & send to QComboBox
-
-def get_devices():
-
-    try:
-        cmd = "adb devices"
-        cmd_out = subprocess.check_output(cmd, shell=True).decode("UTF-8")
-
-        lines = cmd_out.split("\n")
-        devices = lines[1:]
-
-        for device in devices:
-            device = re.sub(r"\tdevice", "", device)
-
-            
-
-            if len(device) > 1:
-                if "offiline" in device:
-                    err_h("Device offline, try to reconnect device.")
-                    pass
-                else:
-                    window.device.addItem(device)
-            else:
-                pass
-        
-
-
-        # If no devices connected, send error to err_h (Line 13)
-
-        if len(window.device) == 0:
-            print("No devices connected")
-            err_h("no devices")
-        
-
-    # if something goes wrong, send the error to "err_h()" (Line 13)
-    except Exception as e:
-        exceptionString = str(e)
-        err_h(exceptionString)
-
-
-
-
-# ==> Show Window & call get_devices function
-def main():
-    window.show()
-
-    while len(window.device) > 0:
-        window.device.removeItem(0)
-
-    get_devices()    
-    
-def advanced_mode_on():
-    window.normal_advanced.setCurrentIndex(1)
-
-
-def advanced_mode_off():
-    window.normal_advanced.setCurrentIndex(0)
-
-
-
-
-# ==> This is the real main function, When "Start" button is pressed, this function will get all window values & send to scrcpy
-def start():
-    device      = window.device.currentText()
-    resolution	= window.resolution.text()
-    bitrate     = window.bitrate.currentText()
-    fps         = window.fps.text()
-    fullscreen  = window.fullscreen.isChecked()
-    view_only   = window.view_only.isChecked()
-    borderless  = window.borderless.isChecked()
-    render      = window.render.currentText()
-    add_args    = window.add_args.text()
-
-
-    code = 'scrcpy '
-
-
-    if (fullscreen == True):
-        code = code + "--fullscreen "
-    if (view_only == True):
-        code = code + "--no-control "
-    if (borderless == True):
-        code = code + "--window-borderless "
-
-
-    try:
-        final_code = f'''{code} --serial {device} -m {resolution} -b {bitrate} --max-fps={fps} --render-driver="{render}" {add_args} &'''
-        os.system(final_code)
-
-    except Exception as e:
-        err_h(str(e))
-        raise e
-
-
-
-    #os.system(final_code)
-
-
-
-
+adb = AdbClient(host="127.0.0.1", port=5037)
 app = QtWidgets.QApplication([])
+
+
+class MainProgram:
+    def startWindow():
+        window.show()
+
+
+    def reZeroCount():
+        while len(window.device) > 0:
+            window.device.removeItem(0)
+
+
+    def get_devices():
+        try:
+            devices = adb.devices()
+
+            if len(devices) == 0:
+                ErrH.noDevices()
+
+            for device in devices:
+                window.device.addItem(device.serial)
+
+        except Exception as e:
+            ErrH(error_str=str(e))
+
+
+    def startScrcpy():
+        device      = window.device.currentText()
+        resolution  = window.resolution.text()
+        bitrate     = window.bitrate.currentText()
+        fps         = window.fps.text()
+        fullscreen  = window.fullscreen.isChecked()
+        view_only   = window.view_only.isChecked()
+        borderless  = window.borderless.isChecked()
+        render      = window.render.currentText()
+        add_args    = window.add_args.text()
+
+        code = 'scrcpy '
+
+        if (fullscreen == True):
+            code = code + "--fullscreen "
+        if (view_only == True):
+            code = code + "--no-control "
+        if (borderless == True):
+            code = code + "--window-borderless "
+
+        try:
+            final_code = f'''{code} --serial {device} -m {resolution} -b {bitrate} --max-fps={fps} --render-driver="{render}" {add_args} &'''
+            os.system(final_code)
+
+        except Exception as e:
+            ErrH(error_str=str(e))
+            #raise e
+
+
+class ErrH:
+    def __init__(self, error_str=None):
+        self.error_str = error_str
+
+        if self.error_str != None:
+            self.showError(error_str)
+
+    def noDevices():
+        error_window.show()
+        error_window.text.setText("No devices connected")
+
+
+    def showError(self, error_str):
+        error_window.show()
+        error_window.text.setText(self.error_str)
+
+
+class WindowManager:
+    def advanced_mode_on():
+        window.normal_advanced.setCurrentIndex(1)
+
+    def advanced_mode_off():
+        window.normal_advanced.setCurrentIndex(0)
+
+def reloadDevices():
+    MainProgram.reZeroCount()
+    MainProgram.get_devices()
+
+def main():
+    MainProgram.startWindow()
+    MainProgram.reZeroCount()
+    MainProgram.get_devices()
 
 
 # Load Windows
@@ -130,18 +106,15 @@ error_window = uic.loadUi("assets/error_window.ui")
 
 
 # Setting functions to window(s) buttons
-window.start.clicked.connect(start)
-window.reload_btn.clicked.connect(main)
-window.advanced_mode.clicked.connect(advanced_mode_on)
-window.normal_mode.clicked.connect(advanced_mode_off)
+window.start.clicked.connect(MainProgram.startScrcpy)
+window.reload_btn.clicked.connect(reloadDevices)
+window.advanced_mode.clicked.connect(WindowManager.advanced_mode_on)
+window.normal_mode.clicked.connect(WindowManager.advanced_mode_off)
 error_window.ok_btn.clicked.connect(error_window.close)
 
-
-
-
-# ==> call the primary function
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-    
+
+
 
 app.exec()
